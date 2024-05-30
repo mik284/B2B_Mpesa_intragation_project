8a0f9b9ffb587abacd67d6cb2554a8afa83d69c3
mik284 <mikeykamau222@gmail.com>
2024-05-30 14:06:19 +0300

    Feat: initial commit

---
+++ src/main/java/com/example/mpesaExpress/MpesaExpressApplication.java
@@ -0,0 +1,13 @@
+package com.example.mpesaExpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpesaExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpesaExpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mpesaExpress/MpesaExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mpesaExpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpesaExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
